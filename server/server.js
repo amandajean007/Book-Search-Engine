8fd995f65eb2f7ce8e56e7ef43b991011702ce7d
Amanda McBee <ahanes35@yahoo.com>
2021-11-19 15:34:54 -0700

    server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const path = require('path');
 const { ApolloServer } = require('@apollo/client');
-
+const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -8,7 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware,
 });
 
 server.applyMiddleware({ app });
@@ -19,8 +21,14 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
